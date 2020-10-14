481a0f02e10bc013f8ed8003f7cc1e1799208b8e
Milica Babic <72348168+suncanjenamesecini@users.noreply.github.com>
2020-10-14 02:49:19 +0200

    Updates

---
+++ precache-manifest.2cbfb30b4aaec8e6837cc6026aafb0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac70093b030e0f08598f00971248c16d",
+    "url": "/mojastranica/index.html"
+  },
+  {
+    "revision": "213bfd170bade301bf05",
+    "url": "/mojastranica/static/css/main.f3566597.chunk.css"
+  },
+  {
+    "revision": "fb852e038a2d77457cd2",
+    "url": "/mojastranica/static/js/2.9ae2ee01.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/mojastranica/static/js/2.9ae2ee01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213bfd170bade301bf05",
+    "url": "/mojastranica/static/js/main.a964d75b.chunk.js"
+  },
+  {
+    "revision": "f4317d88e39762ee6dd7",
+    "url": "/mojastranica/static/js/runtime-main.e11aa717.js"
+  }
+]);
